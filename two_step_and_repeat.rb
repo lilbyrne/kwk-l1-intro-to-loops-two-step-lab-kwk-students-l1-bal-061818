5bde56b35ba8be77d236a81e0e801e6de8a5bc08
Lillian Byrne <lily.byrne.304@gmail.com>
2018-07-02 19:51:27 +0000

    automatically backed up by learn

--- two_step_and_repeat.rb
+++ two_step_and_repeat.rb
@@ -13,3 +13,19 @@ def first_steps
 end
 first_steps
 
+def a_few_more_steps
+   loop do
+    puts "Right foot back"
+    sleep (0.5)
+    puts "Left foot back"
+    sleep (0.5)
+    puts "Right foot back"
+    sleep (0.5)
+    puts "Stop"
+    sleep (0.5)
+    puts "Right foot steps right and back"
+    sleep (0.5)
+    puts ""
+    break
+  end
+  
